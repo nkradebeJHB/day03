b03f4bb7f46c500c7fc98c0d5ed4d5403823bb89
Nkosinathi Radebe <nkradebe@c5r8s3.wethinkcode.co.za>
2020-07-13 08:36:41 +0200

    day03 ex03

--- ex03/ft_div_mod.c
+++ ex03/ft_div_mod.c
@@ -6,7 +6,7 @@
 /*   By: nkradebe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/12 09:35:55 by nkradebe          #+#    #+#             */
-/*   Updated: 2020/07/12 09:37:23 by nkradebe         ###   ########.fr       */
+/*   Updated: 2020/07/13 08:36:30 by nkradebe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@ void	ft_div_mod(int	a, int	b, int	*div, int	*mod)
 {
 	if(b	!=	0)
 	{
-	  *div	=	a / b;
-      *mod	=	a % b;
+	   	*div	=	a / b;
+		*mod	=	a % b;
 	}
 }
 
